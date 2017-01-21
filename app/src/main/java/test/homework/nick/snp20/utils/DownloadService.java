77c4f04ce2e743fb7c7aac6738d96bd6d0f81455
Nick <nick.dnepr@gmail.com>
2017-01-21 23:54:10 +0200

    added view pager

---
+++ app/src/main/java/test/homework/nick/snp20/utils/DownloadService.java
@@ -0,0 +1,7 @@
+package test.homework.nick.snp20.utils;
+
+/**
+ * Created by Nick on 09.12.16.
+ */
+public class DownloadService {
+}
---
+++ app/src/main/java/test/homework/nick/snp20/utils/adapters/MViewPagerAdapter.java
@@ -0,0 +1,7 @@
+package test.homework.nick.snp20.utils.adapters;
+
+/**
+ * Created by Nick on 21.12.16.
+ */
+public class MViewPagerAdapter {
+}
